5e2c9016d355de38991010a9975c486dbddbd585
Pranav Arora <pranavarora0008@gmail.com>
2022-07-28 10:44:37 +0530

    form edited

--- src/App.js
+++ src/App.js
@@ -7,25 +7,31 @@ import Form from 'react-bootstrap/Form';
 function App() {
   return (
     <div className="App">
-     <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
+      <h1>Task</h1>
+      <h2>Receipt Details </h2>
+      <Form>
+      <Form.Group className="mb-3" controlId="formBasicDate">
+        <Form.Label>Date</Form.Label>
+        <Form.Control type="email" placeholder="Enter date"/>
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+      <Form.Group className="mb-3" controlId="formBasicAmount">
+        <Form.Label>Amount</Form.Label>
+        <Form.Control type="Amount" placeholder="Enter Amount (in INR)" />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
+      <Form.Group className="mb-3" controlId="formBasicPaymentMode">
+      <Form.Label>Payment Mode</Form.Label>
+        <Form.Select Amount>
+          <option>Cash</option>
+        </Form.Select>
       </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
+      <Form.Group className="mb-3" controlId="formBasicRemarks">
+      <Form.Label>Remarks</Form.Label>
+        <Form.Control type="Remarks" placeholder="Enter remark"/>
+      </Form.Group>
+    
+      <Button variant="outline-danger">CANCEL(ESC)</Button>{' '}
+      <Button variant="success">SUBMIT </Button>{' '}
     </Form>
     </div>
   );
